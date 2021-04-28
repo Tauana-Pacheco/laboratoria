003ed5ff0485af2876a6d932bc541d004c35b439
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-04-28 17:30:06 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// const name = nome;
+const nome = window.prompt('Seja bem-vinda, jogadora! Qual é o seu nome?')
+
+let decisao =  window.prompt('Deseja continuar com processo?')
+
+        if (decisao =='sim') { 
+            alert(`Você esta no jogo ${nome}`)
+            perguntas();
+        } else {
+            console.log('Até a próxima')
+        }
+
+       function perguntas() {
+
+            const resposta = window.prompt('Quem nasceu em São Paulo é? a) Paulista b)Paulistano c) São Paulino')
+            const resCorreta = 'a';
+
+            if(resposta == resCorreta) {
+
+            } else  {
+                
+            }
+            const resposta1 = window.prompt('Quem nasceu em Rio de Janeiro é? a) Capixaba b)Carioca c) São Paulino')
+            const resCorreta1 = 'b';
+    
+             if(resposta1 == resCorreta1) {
+    
+             }else{
+                 
+             }
+          const resposta2 = window.prompt('Quem nasceu em brasilia norte é? a) Paulista b)Carioca c)brasiliense')
+             const resCorreta2= 'c';
+     
+              if(resposta2 == resCorreta2) {
+     
+            } else {
+
+          }
+         }
